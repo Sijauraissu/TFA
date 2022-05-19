6283e14c589cb96bda1b98b4ffa38fe24fa8cb7d
Sijauraissu <demasy.nathan@gmail.com>
2022-05-19 09:53:59 +0200

    Ajout de l'écriture dans l'html, modification de la prise de valeur dans le tableau

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,13 +2,15 @@
 
 /*   --Carte identité--   */
 
-//  -Variable-  //
+//  -Variables/Constante-  //
 // Variable tableau pour les cartes d'identité
 var tab_identity = [];
 //Ciblage du bouton pour tirer la carte d'identité
 var btn_identity = document.querySelector(".btn_identity");
+//Ciblage de la classe de la carte d'identité
+const selectionClass = document.querySelectorAll(".identity__box--el");
 
-//  -Importation du Json-  //
+//  -Importation du Json dans un tableau-  //
 fetch("./assets/json/json.json")
   .then(
     (response) => {
@@ -27,8 +29,15 @@ btn_identity.addEventListener("click", (e)=>{
   //Random du tableau
   shuffleArray(tab_identity);
   //Prise du dernier élément du tableau
-  var identity = tab_identity.pop();
+  var identity = tab_identity.length-1;
   console.log(identity); //à supprimer
+  //
+  var tab_identitycard = [tab_identity[0].name,tab_identity[0].birthday,tab_identity[0].phone,tab_identity[0].zip,tab_identity[0].city,tab_identity[0].email,];
+  //Ecriture dans l'Html des données du Json
+  for (let i=0; i<selectionClass.length; i++){
+    selectionClass[i].innerHTML = tab_identitycard[i];
+  }
+
   }
 )
 
